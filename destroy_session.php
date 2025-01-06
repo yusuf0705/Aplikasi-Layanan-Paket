6d01474760d5c2089691cad2a67b6109b67cb148
yusuf0705 <muhammadyusufsyafrianis@gmail.com>
2025-01-06 22:37:16 +0700

    Delete destroy_session.php

--- destroy_session.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// menghapus semua session
-session_unset(); // Menghapus varialbel session
-session_destroy(); // Menghancurkan session
-
-echo "Session telah di hapus. <a href='set_session.php'>Set Ulang Session</a>";
-?>
